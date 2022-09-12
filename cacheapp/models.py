996e5893ae8cf51fccad6edd6a9a3d3e2fc0cb7e
Abhishek Mishra <mobappssolutions149@gmail.com>
2022-09-12 16:30:46 +0530

    Initial Commit

--- cacheapp/models.py
+++ cacheapp/models.py
@@ -1,6 +1,8 @@
+from pickle import TRUE
 from django.db import models
 
 # Create your models here.
 class mycache_table(models.Model):
     name=models.CharField(null=True,blank=True)
+    address=models.CharField(null=True,blank=True)
 
